bb37d008056d3aa6f3d61e6c2f21dd0e8ef95080
Steven Swirski <steve.swirski@gmail.com>
2021-05-21 17:28:32 -0400

    Detail button

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,5 +1,9 @@
 const ProductController = require('../controllers/Product.controllers');
 module.exports = app => {
     app.get('/api', ProductController.index);
-    app.post('/api/product', ProductController.create);
+    app.post('/api/product/create', ProductController.create);
+    app.get('/api/product/:id', ProductController.show);
+    app.get('/api/product/update/:id', ProductController.update);
+    app.get('/api/product/delete/:id', ProductController.delete);
+    
 }
