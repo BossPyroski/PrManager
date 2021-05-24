ad733bcdcb68a74bb3f7f89c6826762292fd5c1e
Steven Swirski <steve.swirski@gmail.com>
2021-05-24 13:24:46 -0400

    Edit, update, delete features

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -3,7 +3,7 @@ module.exports = app => {
     app.get('/api', ProductController.index);
     app.post('/api/product/create', ProductController.create);
     app.get('/api/product/:id', ProductController.show);
-    app.get('/api/product/update/:id', ProductController.update);
-    app.get('/api/product/delete/:id', ProductController.delete);
+    app.put('/api/product/update/:id', ProductController.update);
+    app.delete('/api/product/delete/:id', ProductController.delete);
     
 }
